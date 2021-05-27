d5cf95664de2ed0156a9947b311705c938165419
Alejandro Carrillo <37186929@uaz.edu.mx>
2021-05-27 15:24:50 -0500

    Se comenzó a construir el modelo del trámite Atención a migrantes reportads y repatriados

--- atencion_migrantes/models.py
+++ atencion_migrantes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class AtencionMigrantes(models.Model):
+    identificacion_oficial = models.BooleanField("Identificación Oficial")
+    curp = models.BooleanField("CURP")
+    rfc = models.BooleanField("RFC")
+    solicitud_gobernador = models.BooleanField("Solicitud al gobernador")
+    hoja_deportacion = models.BooleanField("Hoja de deportación")
+    comprobante_domicilio = models.BooleanField("Comprobante de domicilio")
+    acta_nacimiento = models.BooleanField("Comprobante de domicilio")
+    
