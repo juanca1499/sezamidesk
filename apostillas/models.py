75cad5891b86df0c2839f4119c4fcfccc51d2652
juca <garciamjuancarlos14@gmail.com>
2021-05-27 11:45:33 -0500

    Se agregaron las urls de la aplicación. Se comenzó con la implementación de vistas y templates

--- apostillas/models.py
+++ apostillas/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Apostilla(models.Model):
+    acta_americana = models.BooleanField('Acta americana',default=False)
+    acta_mexicana = models.BooleanField('Acta mexicana',default=False)
+    identificacion_padres = models.BooleanField('Identificación oficial de ambos padres',default=False)
+    curp = models.BooleanField('CURP',default=False)
+    comprobante_domicilio = models.BooleanField('Comprobante de domicilio',default=False)
+    rfc = models.BooleanField('RFC',default=False)
+    tramite = models.ForeignKey('tramites.Tramite',verbose_name='Tramite',on_delete=models.CASCADE)
--- apostillas/views.py
+++ apostillas/views.py
@@ -2,4 +2,27 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView, TemplateView, View
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import PermissionRequiredMixin
 
+from .models import Apostilla
+
+class ListaApostilla(PermissionRequiredMixin,ListView):
+    model = Apostilla
+
+    def get(self, request, *args, **kwargs):
+        lista_grupos = Apostilla.objects.all()
+
+class NuevaApostilla(PermissionRequiredMixin,CreateView):
+    model = Apostilla
+    success_url = reverse_lazy('apostillas:lista')
+
+class DetalleApostilla(PermissionRequiredMixin,DetailView):
+    model = Apostilla
+
+class EditarApostilla(PermissionRequiredMixin,UpdateView):
+    model = Apostilla
+    success_url = reverse_lazy('apostillas:lista')
+
+class EliminarApostilla(PermissionRequiredMixin,DeleteView):
+    model = Apostilla
+    success_url = reverse_lazy('apostillas:lista')
