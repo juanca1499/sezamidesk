b277a2ccced2e31a93660ef1797d99f73a32e001
Alejandro Carrillo <37186929@uaz.edu.mx>
2021-05-27 12:39:56 -0500

    Se agregó la expresión regular para validar la curp

--- beneficiarios/models.py
+++ beneficiarios/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 # Create your models here.
 class Beneficiario(models.Model):
     
-    curp = models.CharField("CURP", primary_key = True, max_length=18)
+    curp = models.CharField("CURP", primary_key = True, max_length=18, validators=[RegexValidator(r'/^([A-Z][AEIOUX][A-Z]{2}\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])[HM](?:AS|B[CS]|C[CLMSH]|D[FG]|G[TR]|HG|JC|M[CNS]|N[ETL]|OC|PL|Q[TR]|S[PLR]|T[CSL]|VZ|YN|ZS)[B-DF-HJ-NP-TV-Z]{3}[A-Z\d])(\d)$/')])
     primer_apellido = models.CharField("Primer apellido", max_length=50)
     segundo_apellido = models.CharField("Segundo apellido", max_length=50)
     nombre = models.CharField("Nombre(s)", max_length=50)
