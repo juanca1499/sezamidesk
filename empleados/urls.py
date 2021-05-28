38b814dec43d4c559a3b45d70f8d6aa91105b3bd
German-Glez <38192814@uaz.edu.mx>
2021-05-28 08:17:57 -0500

    Nuevo Empleado
    Se creo la view y el template correspondientes a la funcionalidad de agregar un nuevo empleado.

--- empleados/urls.py
+++ empleados/urls.py
@@ -5,5 +5,6 @@ app_name = 'empleados'
 
 urlpatterns = [
     path('lista/', views.EmpleadoList.as_view(), name='lista'),
+    path('nuevo/', views.NuevoEmpleado.as_view(), name='nuevo'),
     
 ]
--- empleados/views.py
+++ empleados/views.py
@@ -1,6 +1,9 @@
 from empleados.models import Empleado
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.views.generic.edit import CreateView
+from .forms import EmpleadoForm
+from django.urls import reverse_lazy
 
 from .models import Empleado
 
@@ -9,4 +12,14 @@ from .models import Empleado
 class EmpleadoList(ListView):
     model = Empleado   
 
-    
+class NuevoEmpleado(CreateView):
+    model = Empleado
+    form_class = EmpleadoForm
+    extra_context = {'etiqueta':'Nuevo','btn':'Agregar'}
+    success_url = reverse_lazy('empleados:lista')
+
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        return super().form_valid(form)
+
+
