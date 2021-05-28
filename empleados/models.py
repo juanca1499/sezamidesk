090ff352b8447dee763e788a9bcde247c1019942
German-Glez <38192814@uaz.edu.mx>
2021-05-28 08:49:03 -0500

    Modelo Empleado
    Se modifico el modelo empleado.

--- empleados/models.py
+++ empleados/models.py
@@ -3,9 +3,10 @@ from django.contrib.auth.models import User
 from django.db.models.fields import CharField 
 
 class Empleado(User):
-    segundo_apellido = models.CharField('Apellido materno',max_length=40,default='Segundo apellido',null=False)
-    alias = models.CharField('Alias',max_length=30,default='alias',null=False)
+    segundo_apellido = models.CharField('Apellido materno',max_length=40,default=False,null=False)
+    alias = models.CharField('Alias',max_length=30,default=False,null=False)
     telefono = models.CharField('Teléfono',max_length=15,null=True)
+    grupo = models.ForeignKey("empleados.Grupo", verbose_name=("Grupo"), on_delete=models.CASCADE)
 
     def __str__(self):
         return self.first_name+' '+self.last_name+' '+self.apellido_materno
@@ -13,3 +14,13 @@ class Empleado(User):
     class Meta:
         verbose_name = 'Empleado'
         verbose_name_plural = 'Empleados'
+
+class Grupo(models.Model):
+    grupo = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.grupo
+
+    class Meta:
+        verbose_name = 'Grupo'
+        verbose_name_plural = 'Grupos'
