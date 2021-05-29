d5fa991680f7e212a8d2a3cad280979d9634f5dc
GabrielCuriel1999 <38192397@uaz.edu.mx>
2021-05-29 13:33:14 -0500

    Se agrego la funcionalidad para editar los datos de las personas desaparecidas

--- localizacion_personas/urls.py
+++ localizacion_personas/urls.py
@@ -5,7 +5,7 @@ app_name = 'localizacion_personas'
 
 urlpatterns = [
     path('lista/', views.ListaPersonasDesaparecidas.as_view(), name='lista'),
-    path('editar/<str:curp>', views.EditarPersonaDesaparecida.as_view(), name='editar'),
+    path('editar/<str:pk>', views.EditarPersonaDesaparecida.as_view(), name='editar'),
     path('nuevo/', views.AgregarPersonaDesaparecida.as_view(), name='nuevo'),
     path('detalle/<str:curp>', views.DetallePersonaDesaparecida.as_view(), name='detalle'),
 ]
