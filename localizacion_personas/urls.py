6071dc7ba219bd8fa67b556f42351266925f8540
GabrielCuriel1999 <38192397@uaz.edu.mx>
2021-05-29 12:23:29 -0500

    Se creó el template que muestra los detalles de las personas desaparecidas

--- localizacion_personas/urls.py
+++ localizacion_personas/urls.py
@@ -5,7 +5,7 @@ app_name = 'localizacion_personas'
 
 urlpatterns = [
     path('lista/', views.ListaPersonasDesaparecidas.as_view(), name='lista'),
-    path('editar/<int:pk>', views.EditarPersonaDesaparecida.as_view(), name='editar'),
+    path('editar/<str:curp>', views.EditarPersonaDesaparecida.as_view(), name='editar'),
     path('nuevo/', views.AgregarPersonaDesaparecida.as_view(), name='nuevo'),
-    path('detalle/<int:pk>', views.DetallePersonaDesaparecida.as_view(), name='detalle'),
+    path('detalle/<str:curp>', views.DetallePersonaDesaparecida.as_view(), name='detalle'),
 ]
