88d35e0f3088b6a075aab6978b036964ff9cdbeb
GabrielCuriel1999 <38192397@uaz.edu.mx>
2021-05-30 11:54:22 -0500

    CHECKPOINT FORMULARIO LOCALIZACION DE PERSONAS

--- localizacion_personas/forms.py
+++ localizacion_personas/forms.py
@@ -16,13 +16,13 @@ class PersonaDesaparecidaForm(forms.ModelForm):
         model = PersonaDesaparecida
         fields = '__all__'
         widgets = {
-                'curp':forms.TextInput(attrs={'class':'form-control','placeholder':'CURP'}),
+                'curp':forms.TextInput(attrs={'class':'form-control','placeholder':'CURP', 'id':'txtrut'}),
                 'nombre_desaparecido':forms.TextInput(attrs={'class':'form-control','placeholder':'Nombre'}),
                 'apellido_paterno_desaparecido':forms.TextInput(attrs={'class':'form-control','placeholder':'Apellido paterno'}),
                 'apellido_materno_desaparecido':forms.TextInput(attrs={'class':'form-control','placeholder':'Apellido materno'}),
                 'fecha_nacimiento_desaparecido':forms.DateInput(attrs={'class':'form-control','placeholder':'Fecha de nacimiento'}),
                 'observaciones':forms.Textarea(attrs={'class':'form-control','placeholder':'Observaciones','rows':4}),
-                'estatus':forms.Select(attrs={'class':'form-control'}),
+                'estatus':forms.Select(attrs={'class':'form-select'}),
                 'ultimo_lugar':forms.TextInput(attrs={'class':'form-control','placeholder':'Último lugar dónde se sabe al desaparecido(a)'}),
                 'fecha_desaparicion':forms.DateInput(attrs={'class':'form-control'}),
                 'telefono_solicitante':forms.TextInput(attrs={'class':'form-control','placeholder':'Número de télefono'}),
