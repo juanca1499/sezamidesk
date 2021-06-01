1a95b039e8c1fb0862bdfc9a88e45876fb033c2a
Alejandro Carrillo <37186929@uaz.edu.mx>
2021-06-01 02:43:17 -0500

    Se cambiaron los campos IntegerField del modelo Beneficiario y se añadió el modal para la eliminación de un beneficiario

--- beneficiarios/urls.py
+++ beneficiarios/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.BeneficiariosPrincipal.as_view(), name = 'principal'),
     path('detalle/<str:pk>', views.BeneficiariosDetalle.as_view(), name = 'detalle'),
     path('editar/<str:pk>', views.BeneficiariosEditar.as_view(), name = 'editar'),
+    path('eliminar/<str:pk>', views.BeneficiariosEliminar.as_view(), name = 'eliminar'),
 ]
